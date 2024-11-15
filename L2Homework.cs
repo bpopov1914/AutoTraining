47b3877a5f2fbf8f04c8c1025258bde939394d72
bpopov1914 <borisbpopov@gmail.com>
2024-11-15 10:44:06 +0200

    Add Task 3

--- L2Homework.cs
+++ L2Homework.cs
@@ -7,3 +7,6 @@ Lecture2HomeworkTask1.PrintSum();
 //Task 2: Format a Full Name
 Lecture2HomeworkTask2.InputNames();
 Lecture2HomeworkTask2.ConcatAndPrintNames();
+
+//Task 3: Extract and Modify Substring
+Lecture2HomeworkTask3.PrintSubstring();
