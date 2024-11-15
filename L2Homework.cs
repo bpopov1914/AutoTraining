34b8b1fe1396444441728cbc51813b9c0833f0c4
bpopov1914 <borisbpopov@gmail.com>
2024-11-15 10:17:51 +0200

    Add Task 2 class and call methods in L2Homework.cs

--- L2Homework.cs
+++ L2Homework.cs
@@ -2,3 +2,7 @@
 
 //Task 1: Convert and Add Two Numbers
 Lecture2HomeworkTask1.PrintTheSumOfTwoNumbers();
+
+//Task 2: Format a Full Name
+Lecture2HomeworkTask2.InputNames();
+Lecture2HomeworkTask2.ConcatAndPrintNames();
