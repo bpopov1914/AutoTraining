bee37dd5c9b809ff2a71295e238d49e420cff4a7
bpopov1914 <borisbpopov@gmail.com>
2024-11-19 18:09:56 +0200

    Task 2 WIP

--- L3HomeworkConsoleApp/Lecture3Homework/Lecture3HomeworkTask2.cs
+++ L3HomeworkConsoleApp/Lecture3Homework/Lecture3HomeworkTask2.cs
@@ -2,5 +2,38 @@ namespace L3HomeworkConsoleApp;
 
 public class Lecture3HomeworkTask2
 {
+    //Task 2: Fishing Trip
     
+    //Variables initialization
+    int groupBudget;
+    string season;
+    int numOfFisherman;
+    double springPrice = 3000.00;
+    double summerPrice = 4200.00;
+    double autumnPrice = 4200.00;
+    double winterPrice = 2600.00;
+    
+    //Prompt the user to input group budget an integer in the range [1…8000]
+    public void InputGroupBudget()
+    {
+        Console.WriteLine("Enter group budget: ");
+        string input = Console.ReadLine();
+        if (int.TryParse(input, out groupBudget))
+        {
+            groupBudget = int.Parse(input);
+            if (groupBudget >= 1 && groupBudget <= 8000)
+            {
+                Console.WriteLine($"You entered group budget: {groupBudget} BGN");
+            }
+            else
+            {
+                Console.WriteLine("Your budget must be between 1 and 8000");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Please enter a valid number input.");
+        }
+    }
+
 }
