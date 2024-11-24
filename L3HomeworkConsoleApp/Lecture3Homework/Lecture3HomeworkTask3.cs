a6eed0e1826a51251889a542a390e31890c013e1
Boris Popov <borisbpopov@gmail.com>
2024-11-24 21:18:55 +0200

    Add Task 3 Input tabs method

--- L3HomeworkConsoleApp/Lecture3Homework/Lecture3HomeworkTask3.cs
+++ L3HomeworkConsoleApp/Lecture3Homework/Lecture3HomeworkTask3.cs
@@ -8,5 +8,45 @@ namespace L3HomeworkConsoleApp.Lecture3Homework
 {
     class Lecture3HomeworkTask3
     {
+        //Task 3: Salary Deduction
+
+        //Variables initialization
+        int numberOfOpenTabs;
+        double salary;
+        string website;
+        string facebook = "Facebook";
+        string instagram = "Instagram";
+        string reddit = "Reddit";
+        double fbFine = 150;
+        double instaFine = 100;
+        double redFine = 50;
+
+        //Input number of open tabs
+        public bool InputNumOfTabs()
+        {
+            Console.WriteLine("Task 3: Please enter the number of tabs. Must be a valid number between 1 and 10.");
+            string input = Console.ReadLine();
+
+            if(int.TryParse(input, out numberOfOpenTabs))
+            {
+                numberOfOpenTabs = int.Parse(input);
+
+                if (numberOfOpenTabs > 0 && numberOfOpenTabs <= 10)
+                {
+                    Console.WriteLine($"The number of tabs is: {numberOfOpenTabs}");
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+
     }
 }
