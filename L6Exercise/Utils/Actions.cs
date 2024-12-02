0bd4a9f813127ca07b7dd034817cdcde60c890e4
bpopov1914 <borisbpopov@gmail.com>
2024-12-02 17:10:25 +0200

    Refactored Actions and Students classes

--- L6Exercise/Utils/Actions.cs
+++ L6Exercise/Utils/Actions.cs
@@ -3,7 +3,6 @@ namespace L6Exercise.Utils;
 public class Actions
 {
     Students students = new Students();
-    UserInput userInput = new UserInput();
     Subjects subjects = new Subjects();
     private int selectedOption;
 
@@ -19,34 +18,19 @@ public class Actions
         {
             case 1:
                 //Add a new student
-                string studentToAdd = userInput.InputStudentName();
-                students.AddStudent(studentToAdd);
+                students.AddStudent();
                 break;
             case 2:
                 //Remove a student
-                string studentToRemove = userInput.InputStudentName();
-                students.RemoveStudent(studentToRemove);
+                students.RemoveStudent();
                 break;
             case 3:
                 //Assign student to subject
-                string studentToUpdate = userInput.InputStudentName();
-                string subject = userInput.InputSubject();
-                bool isSubjectAllowed = subjects.CheckIfSubjectIsAllowed(subject);
-                if (isSubjectAllowed)
-                {
-                    students.AssignStudentToSubject(studentToUpdate, subject);
-                }
-                else
-                {
-                    Console.WriteLine("Invalid subject");
-                }
+                students.AssignStudentToSubject();
                 break;
             case 4:
                 //Add grades to subject
-                string studentToUGrade = userInput.InputStudentName();
-                string subjectToGrade = userInput.InputSubject();
-                List<double> grades = userInput.InputGrade();
-                students.AssignGradesToStudent(studentToUGrade, subjectToGrade, grades);
+                students.AssignGradesToStudent();
                 break;
             case 5:
                 //Display all students
--- L6Exercise/Utils/Students.cs
+++ L6Exercise/Utils/Students.cs
@@ -4,47 +4,50 @@ class Students
 {
     private Dictionary<string, Dictionary<string, List<int>>> students = new Dictionary<string, Dictionary<string, List<int>>>();
     MainMenu mainMenu = new MainMenu();
-    public void AddStudent(string studentName)
+    UserInput userInput = new UserInput();
+    public void AddStudent()
     {
+        string studentToAdd = userInput.InputStudentName();
         //Add check if user already exists
-        students.Add(studentName, new Dictionary<string, List<int>>());
-        Console.WriteLine($"Student {studentName} added successfully!");
-        mainMenu.PrintMainMenu();
+        students.Add(studentToAdd, new Dictionary<string, List<int>>());
+        Console.WriteLine($"Student {studentToAdd} added successfully!");
     }
 
-    public void RemoveStudent(string studentName)
+    public void RemoveStudent()
     {
+        string studentToRemove = userInput.InputStudentName();
         //Add check if user exists
-        students.Remove(studentName);
-        Console.WriteLine($"Student {studentName} removed successfully!");
-        mainMenu.PrintMainMenu();
+        students.Remove(studentToRemove);
+        Console.WriteLine($"Student {studentToRemove} removed successfully!");
     }
 
-    public void AssignStudentToSubject(string studentName, string subject)
+    public void AssignStudentToSubject()
     {
+        string studentToUpdate = userInput.InputStudentName();
+        string subject = userInput.InputSubject();
         //Add assignment logic
-        Console.WriteLine($"Student {studentName} has successfully enrolled in the {subject} class.");
-        mainMenu.PrintMainMenu();
+        Console.WriteLine($"Student {studentToUpdate} has successfully enrolled in the {subject} class.");
     }
 
-    public void AssignGradesToStudent(string studentName, string subject, List<double> grades)
+    public void AssignGradesToStudent()
     {
+        string studentToUGrade = userInput.InputStudentName();
+        string subjectToGrade = userInput.InputSubject();
+        List<double> grades = userInput.InputGrade();
         //Add assignment logic
-        Console.WriteLine($"Student {studentName} has successfully been assigned a grade of {grades} for the {subject} class.");
-        mainMenu.PrintMainMenu();
+        Console.WriteLine($"Student {studentToUGrade} has successfully been assigned a grade of {grades} for the {subjectToGrade} class.");
     }
 
     public void CalculateAverageGrade(Dictionary<string, Dictionary<string, List<int>>> students)
     {
         //Add logic to calculate the final grade
-        mainMenu.PrintMainMenu();
     }
 
     public void DisplayAllStudents()
     {
         //Add logic to display all students, their subjects and the average grade
+        CalculateAverageGrade(students);
         Console.WriteLine(students);
-        mainMenu.PrintMainMenu();
     }
     
 }
