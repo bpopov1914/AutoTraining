d26af70fa19fb20b198d7edf0ae6df8a9c359328
bpopov1914 <borisbpopov@gmail.com>
2025-01-15 11:07:02 +0200

    Inherit interfaces

--- L11Homework/Services/OrderService.cs
+++ L11Homework/Services/OrderService.cs
@@ -1,6 +1,22 @@
+using L11Homework.Interfaces;
+using L11Homework.Models;
+
 namespace L11Homework.Services;
 
-public class OrderService
+public class OrderService :IOrderRepository
 {
-    
+    public void PlaceOrder(Order order)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Order> GetOrders()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Order GetOrderById(int orderId)
+    {
+        throw new NotImplementedException();
+    }
 }
--- L11Homework/Services/ProductService.cs
+++ L11Homework/Services/ProductService.cs
@@ -1,6 +1,32 @@
+using L11Homework.Interfaces;
+using L11Homework.Models;
+
 namespace L11Homework.Services;
 
-public class ProductService
+public class ProductService : IProductRepository
 {
-    
+    public IEnumerable<Product> GetAllProducts()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Product GetProductById(int productId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddProduct(Product product)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateProduct(Product product)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteProduct(int productId)
+    {
+        throw new NotImplementedException();
+    }
 }
