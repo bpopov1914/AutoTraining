bf01e38691edd1522104db1308114bf1187e0698
bpopov1914 <borisbpopov@gmail.com>
2025-01-15 15:33:46 +0200

    WIP: Fix GetAllProducts

--- L11Homework/Repositories/ProductRepository.cs
+++ L11Homework/Repositories/ProductRepository.cs
@@ -1,15 +1,39 @@
 using L11Homework.Interfaces;
 using L11Homework.Models;
+using Npgsql;
 
 namespace L11Homework.Repositories;
 
 public class ProductRepository : IProductRepository
 {
     private Product[] _products = new Product[0];
-    
+    private Database.Database database = new();
     public IEnumerable<Product> GetAllProducts()
     {
-        throw new NotImplementedException();
+        string query = "SELECT * FROM public.\"Products\"\nORDER BY \"ProductId\" ASC";
+        using (var connection = new NpgsqlConnection(database.connectionString))
+        {
+            connection.Open();
+            using (var command = new NpgsqlCommand(query, connection))
+            {
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var index = _products.Length;
+                        var product = new Product();
+                        product.ProductId = reader.GetInt32(0);
+                        product.Name = reader.GetString(1); 
+                        product.Price = reader.GetDouble(2);
+                        product.Stock = reader.GetInt32(3);
+                        
+                        Array.Resize(ref _products, _products.Length + 1);
+                        _products[index] = product;
+                    }
+                }
+            }
+        }
+        return _products;
     }
 
     public Product GetProductById(int productId)
@@ -19,7 +43,7 @@ public class ProductRepository : IProductRepository
 
     public void AddProduct(Product product)
     {
-        _products.Append(product);
+        
     }
 
     public void UpdateProduct(Product product)
--- L11Homework/Services/ProductService.cs
+++ L11Homework/Services/ProductService.cs
@@ -7,32 +7,13 @@ namespace L11Homework.Services;
 
 public class ProductService : ProductRepository
 {
-    Database.Database database = new();
     ProductRepository productRepo = new();
     public void GetAllProducts()
     {
-        string query = "SELECT * FROM public.\"Products\"\nORDER BY \"ProductId\" ASC";
-        using (var connection = new NpgsqlConnection(database.connectionString))
+        var products = productRepo.GetAllProducts();
+        foreach (var product in products)
         {
-            connection.Open();
-            using (var command = new NpgsqlCommand(query, connection))
-            {
-                using (var reader = command.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        var product = new Product();
-                        product.ProductId = reader.GetInt32(0);
-                        product.Name = reader.GetString(1); 
-                        product.Price = reader.GetDouble(2);
-                        product.Stock = reader.GetInt32(3);
-                        productRepo.AddProduct(product);
-                        Console.WriteLine($"ProductId: {product.ProductId }, Name: {product.Name}, Price: {product.Price}, Stock: {product.Stock}");
-                    }
-                }
-            }
-
-        
+            Console.WriteLine($"ProductId: {product.ProductId }, Name: {product.Name}, Price: {product.Price}, Stock: {product.Stock}");
         }
     }
 }
