96dd8f48255154dd77237790c0b0c7b32995675f
bpopov1914 <borisbpopov@gmail.com>
2025-01-21 09:53:43 +0200

    T1: Common elements

---
+++ L4Homework/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace L4Homework;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Task 1: Common elements
+        
+        string[] arrayOne = { "Hey", "hello", "test", "2", "4"};
+        string[] arrayTwo = { "10","test", "hey", "4", "hello", "no" };
+
+        for (int i = 0; i < arrayOne.Length; i++)
+        {
+            for (int j = 0; j < arrayTwo.Length; j++)
+            {
+                if (arrayOne[i] == arrayTwo[j])
+                {
+                    Console.Write($"{arrayOne[i]} ");
+                }
+            }
+        }
+    }
+}
